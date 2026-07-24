cc275cce69edbee56b4eb41064a9b4fd92ffb68c
Frederic GODI <fredericgodi@gmail.com>
2026-07-24 19:50:44 -0400

    Initial commit

---
+++ element.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ ! "$1" ]]
+then
+  echo Please provide an element as an argument.
+else
+then
+ # Identify argument type
+
+ # Get element data, join properties data
+
+ # Format response
+
+ # Display response
+fi
