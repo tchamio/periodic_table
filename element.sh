c4c9a6afe267ca3f01777026e8b9183a75606b8a
Frederic GODI <fredericgodi@gmail.com>
2026-07-27 16:46:32 -0400

    fix: show 404 for none registered element

--- element.sh
+++ element.sh
@@ -26,10 +26,15 @@ else
     where elements.atomic_number = $1;"
     )
 
-    echo "$ELEMENT_RESULT" | sed -E s'/[ | ]/ /g' | while IFS=" " read id symb name mass melting boiling type
-    do
-      echo "The element with atomic number $id is $name($symb). It's a $type, with a mass of $mass amu. $name has a melting point of $melting celsius and a boiling point of $boiling celsius."
-    done
+    if [[ -z "$ELEMENT_RESULT" ]]
+    then
+      echo "There is not such atomic number in our database"
+    else
+      echo "$ELEMENT_RESULT" | sed -E s'/[ | ]/ /g' | while IFS=" " read id symb name mass melting boiling type
+      do
+        echo "The element with atomic number $id is $name($symb). It's a $type, with a mass of $mass amu. $name has a melting point of $melting celsius and a boiling point of $boiling celsius."
+      done
+    fi
   else
     STR_LENGTH=$(echo -n $1 | wc -m)
     if [[ "$1" =~ ^[A-Z]{1}[a-z]{0,2} && $STR_LENGTH -le 2 ]] 
@@ -49,10 +54,15 @@ else
         left join types on properties.type_id = types.type_id 
         where elements.symbol = '$1';"
       )
-      echo "$ELEMENT_RESULT" | sed -E s'/[ | ]/ /g' | while IFS=" " read id symb name mass melting boiling type
-      do
-        echo "The element with atomic number $id is $name($symb). It's a $type, with a mass of $mass amu. $name has a melting point of $melting celsius and a boiling point of $boiling celsius."
-      done
+      if [[ -z "$ELEMENT_RESULT" ]]
+      then
+        echo "The element with that symbol is not registered in our database"
+      else
+        echo "$ELEMENT_RESULT" | sed -E s'/[ | ]/ /g' | while IFS=" " read id symb name mass melting boiling type
+        do
+          echo "The element with atomic number $id is $name($symb). It's a $type, with a mass of $mass amu. $name has a melting point of $melting celsius and a boiling point of $boiling celsius."
+        done
+      fi
     else 
       echo "search by name"
       ELEMENT_RESULT=$($PSQL "select 
@@ -68,10 +78,15 @@ else
         left join types on properties.type_id = types.type_id 
         where elements.name = '$1';"
       )
-      echo "$ELEMENT_RESULT" | sed -E s'/[ | ]/ /g' | while IFS=" " read id symb name mass melting boiling type
-      do
-        echo "The element with atomic number $id is $name($symb). It's a $type, with a mass of $mass amu. $name has a melting point of $melting celsius and a boiling point of $boiling celsius."
-      done
+      if [[ -z "$ELEMENT_RESULT" ]]
+      then
+        echo "The element with that name is not registered in our database"
+      else
+        echo "$ELEMENT_RESULT" | sed -E s'/[ | ]/ /g' | while IFS=" " read id symb name mass melting boiling type
+        do
+          echo "The element with atomic number $id is $name($symb). It's a $type, with a mass of $mass amu. $name has a melting point of $melting celsius and a boiling point of $boiling celsius."
+        done
+      fi
     fi  
   fi 
 fi
